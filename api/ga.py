fe46655f5c521c128e69ae4e24ef26512a91fe44
Seoyoung Jeong <>
2018-09-12 15:12:13 +0900

    API 모듈 분리, 포스트 페이지뷰

---
+++ api/ga.py
@@ -0,0 +1,180 @@
+"""Google Analytics Reporting API V4.
+
+Creating a Report
+https://developers.google.com/analytics/devguides/reporting/core/v4/basic
+
+Dimensions & Metrics Explorer
+https://developers.google.com/analytics/devguides/reporting/core/dimsmets
+"""
+
+from flask import Flask, jsonify, Blueprint, request
+
+from googleapiclient.discovery import build
+from oauth2client.service_account import ServiceAccountCredentials
+import re
+import operator
+
+SCOPES = ['https://www.googleapis.com/auth/analytics.readonly']
+KEY_FILE_LOCATION = './blog-api-215703.json'
+VIEW_ID = '136669174'
+
+
+def initialize_analyticsreporting():
+    """Initializes an Analytics Reporting API V4 service object.
+
+    Returns:
+      An authorized Analytics Reporting API V4 service object.
+    """
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(
+        KEY_FILE_LOCATION, SCOPES)
+
+    # Build the service object.
+    analytics = build('analyticsreporting', 'v4', credentials=credentials)
+
+    return analytics
+
+
+def get_pageview(analytics, startDate='7daysAgo', endDate='today'):
+    """페이지뷰"""
+
+    batchData = analytics.reports().batchGet(
+        body={
+            'reportRequests': [
+                {
+                    'viewId': VIEW_ID,
+                    'dateRanges': [{'startDate': startDate, 'endDate': endDate}],
+                    "metrics": [
+                        {
+                            "expression": "ga:pageviews",
+                        },
+                    ],
+                    "dimensions": [
+                        {
+                            "name": "ga:pagePath",
+                        },
+                    ],
+                    "orderBys": [
+                        {"fieldName": "ga:pageviews", "sortOrder": "DESCENDING"},
+                    ]
+                }]
+        }
+    ).execute()
+
+    rows = batchData.get('reports', [])[0].get('data', {}).get('rows', [])
+
+    print('rows', rows)
+
+    postPage = {}
+
+    MIN_COUNT = 10
+
+    for row in rows:
+        # ^\/posts\/(?!\/)[가-힣\w-]+
+        pagePath = row.get('dimensions', [])[0].lower()
+        count = int(row.get('metrics')[0].get('values')[0])
+
+        # /post 로 시작하고, 역슬래쉬(\)를 포함하지 않는 문자열을 매칭
+        postPathRegex = re.compile('^\/posts\/(?!\/)[가-힣\w-]+')
+        matchingPostPath = postPathRegex.match(pagePath)
+
+        # 포스트 라우팅이 매칭되었을 때
+        if postPathRegex.match(pagePath) and count > MIN_COUNT:
+            print(matchingPostPath.group())
+            pagePath = matchingPostPath.group()
+
+            # if pagePath not in pagePathList:
+            if pagePath not in postPage:
+                postPage[pagePath] = {
+                    'count': count
+                }
+            else:
+                postPage[pagePath]['count'] = postPage[pagePath]['count'] + count
+
+    # 결과 리스트 생성
+    result = []
+    for page in postPage.keys():
+        result.append({
+            'page': page,
+            'count': postPage[page]['count']
+        })
+        print(page)
+        pass
+
+    result = sorted(result, key=lambda k: k['count'], reverse=True)
+
+    return result
+
+
+def get_report_example(analytics):
+    batchData = analytics.reports().batchGet(
+        body={
+            'reportRequests': [
+                {
+                    'viewId': VIEW_ID,
+                    'dateRanges': [{'startDate': '7daysAgo', 'endDate': 'today'}],
+                    'metrics': [{'expression': 'ga:sessions'}],
+                    'dimensions': [{'name': 'ga:country'}]
+                }]
+        }
+    ).execute()
+
+    data = batchData.get('reports', [])[0].get('data', {})
+
+    print(data)
+
+    res = {}
+
+    return res
+
+
+def print_response(response):
+    """Parses and prints the Analytics Reporting API V4 response.
+
+    Args:
+      response: An Analytics Reporting API V4 response.
+    """
+    for report in response.get('reports', []):
+        columnHeader = report.get('columnHeader', {})
+        dimensionHeaders = columnHeader.get('dimensions', [])
+        metricHeaders = columnHeader.get(
+            'metricHeader', {}).get('metricHeaderEntries', [])
+
+        for row in report.get('data', {}).get('rows', []):
+            dimensions = row.get('dimensions', [])
+            dateRangeValues = row.get('metrics', [])
+
+            for header, dimension in zip(dimensionHeaders, dimensions):
+                print(header + ': ' + dimension)
+
+            for i, values in enumerate(dateRangeValues):
+                print('Date range: ' + str(i))
+                for metricHeader, value in zip(metricHeaders, values.get('values')):
+                    print(metricHeader.get('name') + ': ' + value)
+
+
+def jsonRes(res):
+    """response 데이터를 jsonify 모듈로 변환해서 전달"""
+    return jsonify(res), 200, {'Content-Type': 'application/json; charset=utf-8'}
+
+
+ga_api = Blueprint('ga_api', __name__)
+analytics = initialize_analyticsreporting()
+
+
+@ga_api.route('/api/ga/post_pageviews')
+def post_pageview():
+    print(request.args)
+    startDate = request.args.get('startDate')
+    endDate = request.args.get('endDate')
+
+    response = get_pageview(analytics, startDate, endDate)
+    # print(response['reports'][0]['data'])
+
+    return jsonRes(response)
+
+
+@ga_api.route('/api/ga/example')
+def defaultReport():
+    response = get_report_example(analytics)
+
+    return jsonRes(response)
--- app.py
+++ app.py
@@ -1,25 +1,10 @@
 from flask import Flask, jsonify
-from analytics import initialize_analyticsreporting, get_pageview, get_report_example, get_pageview, print_response
+from api.ga import ga_api
 
 app = Flask(__name__)
-analytics = initialize_analyticsreporting()
+app.register_blueprint(ga_api)
 
 
 @app.route('/')
 def index():
     return 'blog-api'
-
-
-@app.route('/report/visitors')
-def report_visitors():
-    response = get_pageview(analytics)
-    # print(response['reports'][0]['data'])
-
-    return jsonify(response), 200, {'Content-Type': 'application/json; charset=utf-8'}
-
-
-@app.route('/report/example')
-def defaultReport():
-    response = get_report_example(analytics)
-
-    return jsonify(response)
